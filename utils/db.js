779f4e4518719a0885256ac4ba4c531a092d5f91
EchoFUN <xukai.ken@gmail.com>
2014-09-20 00:25:05 +0800

    完善数据库层的设置

--- utils/db.js
+++ utils/db.js
@@ -4,5 +4,21 @@
  *
  */
 
+var mysql = require('mysql'), dbConfig = require('../config/db');
+
+var dbConntionPool;
 
 
+
+
+exports.getConnection = function() {
+  
+  // 创建连接池链接
+  if (dbConntionPool) {
+    return this;
+  } else {
+    dbConntionPool = mysql.createPool(dbConfig);
+  }
+  
+  
+};
