fe3a9b1d5958f6dadb8916a5ac5f561fb60bc4a1
EchoFUN <xukai.ken@gmail.com>
2014-09-21 16:17:50 +0800

    调试数据库的链接设置！

---
+++ configs/db.js
@@ -0,0 +1,21 @@
+/**
+ * 数据库的连接配置文件
+ *
+ * @author Kai.XU(xukai.ken@gmail.com)
+ * @date 2014-09-19 星期五
+ */
+
+var dbConfig = {
+  
+  // 服务器路径
+  'host': 'localhost',
+  
+  // 账户名称，以及密码
+  'user': 'root',
+  
+  // 数据库连接名称
+  'database': 'learn'
+  
+};
+
+exports = dbConfig;
