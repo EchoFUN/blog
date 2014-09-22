93e73864042aa32cbc07cdb441c5fef1d05221c2
EchoFUN <xukai.ken@gmail.com>
2014-09-22 15:44:56 +0800

    添加文案设置

--- public/javascripts/module/general.js
+++ public/javascripts/module/general.js
@@ -1,13 +1,22 @@
 /**
- * 
+ *
  *
  * @author Kai.XU(xukai.ken@gmail.com)
  * @date 2014-09-22 星期一
  *
  */
 
-define(['lib/jquery'], function() {
+define(['lib/jquery'], function ($) {
   
-  
-  
+  // 顶部闪烁的光标样式
+  var jQflash = $('#flashLite')
+  setInterval(function () {
+    var content = $.trim(jQflash.text());
+    if (content) {
+      jQflash.text('');
+    } else {
+      jQflash.text('_');
+    }
+  }, 500);
+
 });
