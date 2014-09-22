4171858e02bd5f85649533bb99c0515bc6ce6aba
EchoFUN <xukai.ken@gmail.com>
2014-09-22 16:41:40 +0800

    更新文件包

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+// app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
