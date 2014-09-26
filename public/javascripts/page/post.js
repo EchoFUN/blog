d838ba8727ef92702d96810846359098603c9a75
EchoFUN <xukai.ken@gmail.com>
2014-09-26 22:56:14 +0800

    你好世界！

--- public/javascripts/page/post.js
+++ public/javascripts/page/post.js
@@ -5,4 +5,17 @@ require.config({
 
 require(['lib/jquery', 'module/general'], function() {
   
+  var jQrespond = $('#respond-form');
+  
+  // 在提交订单的时候触发的事件
+  var submitHandler = function(evt) {
+    evt.preventDefault();
+  };
+  
+  
+  var bindEvt = function() {
+    jQrespond.submit(submitHandler);
+  };
+  bindEvt();
+  
 });
