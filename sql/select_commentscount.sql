c679f000e084095c2393034e8c64f67c0f5a6f31
EchoFUN <xukai.ken@gmail.com>
2014-10-03 23:40:11 +0800

    完善SQL中的错误

--- sql/select_commentscount.sql
+++ sql/select_commentscount.sql
@@ -1,1 +1,1 @@
-select pid, count(*) count from comment where approved = 1 and pid in(<%= pids %>) group by pid;
+select pid, count(*) count from comment where approved = 1 and pid in(<%if (pids.length != 0) { %><%= pids %><% } else { %>0<% } %>) group by pid;
